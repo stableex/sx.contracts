10156213126f668451b936f3e097c868fe5a86a3
Denis <carriere.denis@gmail.com>
2020-06-14 15:43:46 -0400

    fix indents

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -11,17 +11,17 @@ using namespace std;
 class [[eosio::contract]] basic : public contract {
 
 public:
-	using contract::contract;
+    using contract::contract;
 
-	[[eosio::action]]
-	void init( const asset quantity, const symbol_code symcode )
-	{
-		// pre-calculate rate
+    [[eosio::action]]
+    void init( const asset quantity, const symbol_code symcode )
+    {
+        // pre-calculate rate
         const asset rate = swapSx::get_rate("swap.sx"_n, quantity, symcode);
-		print("rate: " + rate.to_string() );
+        print("rate: " + rate.to_string() );
 
         // swap
         token::transfer_action transfer( "eosio.token"_n, { get_self(), "active"_n });
-		transfer.send( get_self(), "swap.sx"_n, quantity, symcode.to_string() );
-	}
+        transfer.send( get_self(), "swap.sx"_n, quantity, symcode.to_string() );
+    }
 };
--- examples/entry.cpp
+++ examples/entry.cpp
@@ -9,29 +9,29 @@ using namespace std;
 class [[eosio::contract]] entry : public contract {
 
 public:
-	using contract::contract;
+    using contract::contract;
 
-	[[eosio::action]]
-	void init( const asset quantity )
-	{
+    [[eosio::action]]
+    void init( const asset quantity )
+    {
         token::transfer_action transfer( "eosio.token"_n, { get_self(), "active"_n });
-		transfer.send( get_self(), "swap.sx"_n, quantity, "B" );
-	}
+        transfer.send( get_self(), "swap.sx"_n, quantity, "B" );
+    }
 
-	[[eosio::on_notify("eosio.token::transfer")]]
-	void on_transfer( const name from, const name to, const asset quantity, const string memo )
-	{
+    [[eosio::on_notify("eosio.token::transfer")]]
+    void on_transfer( const name from, const name to, const asset quantity, const string memo )
+    {
         if ( to != get_self() ) return;
-		if ( quantity.symbol.code() != symbol_code{"B"} ) return;
+        if ( quantity.symbol.code() != symbol_code{"B"} ) return;
 
         // re-rentry with incoming transfer
-		token::transfer_action transfer( "eosio.token"_n, { get_self(), "active"_n });
+        token::transfer_action transfer( "eosio.token"_n, { get_self(), "active"_n });
 
-		// split into 4 requests
-		const asset partial = quantity / 4;
-		transfer.send( get_self(), "swap.sx"_n, partial, "A" );
-		transfer.send( get_self(), "swap.sx"_n, partial, "A" );
-		transfer.send( get_self(), "swap.sx"_n, partial, "A" );
-		transfer.send( get_self(), "swap.sx"_n, partial, "A" );
-	}
+        // split into 4 requests
+        const asset partial = quantity / 4;
+        transfer.send( get_self(), "swap.sx"_n, partial, "A" );
+        transfer.send( get_self(), "swap.sx"_n, partial, "A" );
+        transfer.send( get_self(), "swap.sx"_n, partial, "A" );
+        transfer.send( get_self(), "swap.sx"_n, partial, "A" );
+    }
 };
