04a3b64ae64adaabd17f5d23bbcbbbcacebedb8b
Denis <carriere.denis@gmail.com>
2020-10-15 14:11:04 -0400

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-eosio-cpp swap.sx.cpp -I include
+eosio-cpp swap.sx.cpp -I ../
 cleos set contract swap.sx . swap.sx.wasm swap.sx.abi
 
-eosio-cpp examples/entry.cpp -I include
+eosio-cpp examples/entry.cpp -I ../
 cleos set contract entry . entry.wasm entry.abi
 
-eosio-cpp examples/basic.cpp -I include
+eosio-cpp examples/basic.cpp -I ../
 cleos set contract basic . basic.wasm basic.abi
